fa666cacf57ff4e66ffe4d57f9b5fec241da4dbf
kanemototomoki <kanemototomoki@gmail.com>
2022-06-29 22:52:05 +0900

    update: package

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from '@src/App'
+import App from '@src/pages/App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App />)
---
+++ src/pages/App/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <main
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      <h1>recoil test</h1>
+    </main>
+  )
+}
+
+export default App
